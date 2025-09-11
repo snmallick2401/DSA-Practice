ac3f9e119cc277c5a697cc49885fe723b1b4a163
Suhaib Nayyar Mallick <135153061+snmallick2401@users.noreply.github.com>
2025-09-11 15:38:22 +0530

    Update 01_Reverse_a_Number.cpp

--- 01 the basics/01 Basic Maths/01_Reverse_a_Number.cpp
+++ 01 the basics/01 Basic Maths/01_Reverse_a_Number.cpp
@@ -6,21 +6,14 @@ int main() {
     cout << "Write the number you want to reverse : " << endl;
     cin >> x;
 
-    int ans = 0;
+    long long ans = 0;
     while (x != 0) {
-        int d = x % 10;
-        if (ans > INT_MAX / 10 || (ans == INT_MAX / 10 && d > 7)) {
-            cout << "Overflow! Cannot reverse." << endl;
-            return 0;
-        }
-        if (ans < INT_MIN / 10 || (ans == INT_MIN / 10 && d < -8)) {
-            cout << "Underflow! Cannot reverse." << endl;
-            return 0;
-        }
-
+        long long d = x % 10;
         ans = ans * 10 + d;
         x /= 10;
     }
+
+    if (ans < INT_MIN || ans > INT_MAX) return 0;
 
     cout << "Reversed number : " << ans << endl;
     return 0;
